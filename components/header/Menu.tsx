0a9b3c7471cc0b7be51a987a3bc2cf332c9706bb
Meghan Bomberger <44277473+MeghanBomberger@users.noreply.github.com>
2026-03-17 00:24:10 -0600

    menu

--- components/header/Menu.tsx
+++ components/header/Menu.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, Animated } from 'react-native';
 import { colors } from '../../utils/colors';
 
@@ -8,14 +8,26 @@ interface MenuProps {
 }
 
 export default function Menu({ isMenuOpen, setIsMenuOpen }: MenuProps) {
+
+  const rightAnim = useRef(new Animated.Value(isMenuOpen ? 0 : -400)).current;
+
+  useEffect(() => {
+    Animated.timing(rightAnim, {
+      toValue: isMenuOpen ? 0 : -400,
+      duration: 400,
+      useNativeDriver: false,
+    }).start();
+  }, [isMenuOpen]);
+
   const handleLogout = () => {
-    // Implement logout logic here
     setIsMenuOpen(false);
   };
 
   return (
-    <Animated.View style={[styles.menuContainer, { right: isMenuOpen ? 0 : -400 }]}
-      pointerEvents={isMenuOpen ? 'auto' : 'none'}>
+    <Animated.View
+      style={[styles.menuContainer, { right: rightAnim }]}
+      pointerEvents={isMenuOpen ? 'auto' : 'none'}
+    >
       <View style={[styles.menu, { transform: [{ rotate: isMenuOpen ? '-17.5deg' : '0deg' }] }]}>
         <TouchableOpacity onPress={handleLogout}>
           <Text style={styles.menuText}>LOGOUT</Text>
@@ -33,7 +45,6 @@ const styles = StyleSheet.create({
     width: '100%',
     zIndex: 15,
     backgroundColor: colors.yellowStickyNote,
-    transition: 'right 1s',
   },
   menu: {
     position: 'absolute',
@@ -41,7 +52,7 @@ const styles = StyleSheet.create({
     right: 0,
     height: 240,
     width: 240,
-    shadowColor: '#000',
+    shadowColor: colors.shadow,
     shadowOffset: { width: 8, height: 8 },
     shadowOpacity: 0.2,
     shadowRadius: 8,
