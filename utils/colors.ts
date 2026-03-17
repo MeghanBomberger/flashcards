25513b2d51f6ed4dac5a35b92e4011de481c0f35
Meghan Bomberger <44277473+MeghanBomberger@users.noreply.github.com>
2026-03-17 00:19:09 -0600

    theme colors

--- utils/colors.ts
+++ utils/colors.ts
@@ -1,3 +1,7 @@
+	pinkStickyNote: 'rgb(241, 126, 176)',
+	greenStickyNote: 'rgb(175, 247, 42)',
+	orangeStickyNote: 'rgb(252, 183, 18)',
+	blueStickyNote: 'rgb(84, 205, 242)',
 export const colors = {
 	bgPaper: 'rgb(255, 250, 235)',
 	borderRed: 'rgba(205, 0, 50, .5)',
@@ -5,4 +9,5 @@ export const colors = {
 	lineBlue: 'rgba(0, 150, 200, 0.5)',
 	shadow: 'rgba(0,0,0,0.25)',
 	pencilDark: 'rgba(50, 50, 50, 0.95)',
+	yellowStickyNote: 'rgb(248, 229, 58)',
 };
